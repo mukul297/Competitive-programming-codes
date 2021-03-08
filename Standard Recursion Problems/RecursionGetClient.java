5101b08ccf66fc46614d6e2ba83c424d6ee322ab
Mukul Sharma <43707359+mukul297@users.noreply.github.com>
2021-03-08 09:36:28 +0530

    Create RecursionGetClient.java

---
+++ Standard Recursion Problems/RecursionGetClient.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public class RecursionGetClient {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> list = new ArrayList<>();
+		String s = "";
+		list.add(s);
+
+		String str = "hello";
+		System.out.println(str.charAt(0));
+		String ss = str.substring(1);
+
+	}
+
+}
